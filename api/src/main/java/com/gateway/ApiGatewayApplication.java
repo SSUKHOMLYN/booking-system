eadca48ebc0bc95c7fc6d0457f3891f73749333f
Emmanouil Zervakis <115080541+ezervakis@users.noreply.github.com>
2025-01-24 21:33:20 +0200

    Fix

---
+++ api/src/main/java/com/gateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+        public static void main(String[] args) {
+                SpringApplication.run(ApiGatewayApplication.class, args);
+        }
+
+        @Bean
+        public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route("auth-route", r -> r.path("/auth/**")
+                                                .uri("lb://auth-server"))
+                                .route("slots-route", r -> r.path("/slots/**")
+                                                .uri("lb://appointments-server"))
+                                .route("admin-route", r -> r.path("/admin/**")
+                                                .uri("lb://appointments-server"))
+                                .build();
+        }
+}
