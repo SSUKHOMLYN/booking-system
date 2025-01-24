b5cfe154eebba2a338056ab18386a057651489cd
Emmanouil Zervakis <115080541+ezervakis@users.noreply.github.com>
2025-01-25 00:26:20 +0200

    fix

--- api/src/main/java/com/gateway/config/CorsConfig.java
+++ api/src/main/java/com/gateway/config/CorsConfig.java
@@ -1,24 +1,29 @@
 package com.gateway.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+// Notice that CorsConfiguration is imported from `org.springframework.web.cors`...
+import org.springframework.web.cors.CorsConfiguration;
+
+// ...but these two come from `org.springframework.web.cors.reactive`.
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+
+        return new CorsWebFilter(source);
     }
 }
