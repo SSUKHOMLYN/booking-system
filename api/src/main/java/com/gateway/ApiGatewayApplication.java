f5a1fea2fd88e0308400ecfbcf457768209f4461
Emmanouil Zervakis <115080541+ezervakis@users.noreply.github.com>
2025-01-25 09:03:28 +0200

    fix

--- api/src/main/java/com/gateway/ApiGatewayApplication.java
+++ api/src/main/java/com/gateway/ApiGatewayApplication.java
@@ -8,19 +8,19 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
-        public static void main(String[] args) {
-                SpringApplication.run(ApiGatewayApplication.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
-        @Bean
-        public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-                return builder.routes()
-                                .route("auth-route", r -> r.path("/auth/**")
-                                                .uri("lb://auth-server"))
-                                .route("slots-route", r -> r.path("/slots/**")
-                                                .uri("lb://appointments-server"))
-                                .route("admin-route", r -> r.path("/admin/**")
-                                                .uri("lb://appointments-server"))
-                                .build();
-        }
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-route", r -> r.path("/auth/**")
+                        .uri("https://cc-book-app-735691842d39.herokuapp.com")) // Replace with the Auth service URL
+                .route("slots-route", r -> r.path("/slots/**")
+                        .uri("https://cc-book-app-735691842d39.herokuapp.com")) // Replace with the Appointments service URL
+                .route("admin-route", r -> r.path("/admin/**")
+                        .uri("https://cc-book-app-735691842d39.herokuapp.com")) // Replace with the Admin route URL
+                .build();
+    }
 }
